6d402310e440053ab5c9633b9566cc64c302001d
Stephanie Challita <stephanie.challita@inria.fr>
2017-10-02 00:02:32 +0200

    Add some TOSCA2OCCI mappings

--- src/org/occiware/clouddesigner/tosca/handlers/Main.java
+++ src/org/occiware/clouddesigner/tosca/handlers/Main.java
@@ -3,14 +3,18 @@ package org.occiware.clouddesigner.tosca.handlers;
 
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 
 import org.eclipse.cmf.occi.core.Attribute;
 import org.eclipse.cmf.occi.core.DataType;
+import org.eclipse.cmf.occi.core.EObjectType;
 import org.eclipse.cmf.occi.core.Extension;
 import org.eclipse.cmf.occi.core.Kind;
+import org.eclipse.cmf.occi.core.Mixin;
 import org.eclipse.cmf.occi.core.OCCIFactory;
 import org.eclipse.cmf.occi.core.util.OcciHelper;
 import org.eclipse.core.commands.AbstractHandler;
@@ -34,32 +38,23 @@ public class Main extends AbstractHandler {
 	
 	public static Extension extension;
 	public static Kind resourceKind;
+	public static Kind linkKind;
 	public static Resource resource;
 	
-	public static void init() {
-		ResourceSet resSet = new ResourceSetImpl();
-		URI modelURI = URI.createURI("file:/C:/Users/schallit/runtime-EclipseApplication31072017/tosca_model/TOSCA.occie");
-		resource = resSet.createResource(modelURI);
+	public static Extension coreExtension;
+	
+	public static Extension infrastructureExtension;
+	
+	public static Extension platformExtension;
+	
+	public static Kind storageResourceInfrastructure;
+	
+	public static Kind componentResourcePlatform;
 
-		extension = OCCIFactory.eINSTANCE.createExtension();
-		extension.setDescription("Mon extension TOSCA");
-		extension.setScheme("http://tosca/occi#");
-		extension.setName("TOSCA");
-		resource.getContents().add(extension);
-
-		Extension coreExtension = OcciHelper.loadExtension("http://schemas.ogf.org/occi/core#");
-		if (coreExtension == null) {
-			throw new RuntimeException("Cannot load OCCI core extension!");
-		}
-		System.out.println(coreExtension.getTypes());
-		List<Kind> coreKinds = coreExtension.getKinds();
-		for (Kind coreKind : coreKinds) {
-			if (coreKind.getTerm().equals("resource")) {
-				resourceKind = coreKind;
-			}
-		}
-	}
-
+	private Map<String, ?> kinds;
+	
+	private Map<String, ?> map;
+	
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
 		IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindowChecked(event);
@@ -68,15 +63,30 @@ public class Main extends AbstractHandler {
 		try {
 			YamlReader reader = new YamlReader(new FileReader(
 					"C:/Users/schallit/workspace-tosca/org.occiware.clouddesigner.tosca/normative-types.yml"));
-			Map<String, ?> map = (Map<String, ?>)reader.read();
+			map = (Map<String, ?>)reader.read();
+			
+			// links / relationship
+			kinds = (Map<String, ?>) map.get("relationship_types");
+    		for (String kind : kinds.keySet()) {
+    			readKind(kind, (Map<String, ?>) kinds.get(kind));
+    		}
+    		
+    		// datatypes <=> record
+    		Map<String, ?> dataTypes = (Map<String, ?>) map.get("data_types");
+    		for (String datatype : dataTypes.keySet()) {
+    			DataTypeReader.read(datatype, (Map<String, ?>) dataTypes.get(datatype));
+    		}
+    		
 		    for(String collection : map.keySet()) {
-		    	if (collection.endsWith("_types")) {
-		    		Map<String, ?> kinds = (Map<String, ?>) map.get(collection);
+		    	if (collection.endsWith("_types") && !"relationship_types".equals(collection)
+		    			&& !"data_types".equals(collection)) {
+		    		kinds = (Map<String, ?>) map.get(collection);
 		    		for (String kind : kinds.keySet()) {
 		    			readKind(kind, (Map<String, ?>) kinds.get(kind));
 		    		}
 		    	}
 		    }
+		    
 		} catch (Exception e) {
 			throw new RuntimeException(e);
 		}
@@ -92,10 +102,112 @@ public class Main extends AbstractHandler {
 		return null;
 	}
 	
+	public static void init() {
+		ResourceSet resSet = new ResourceSetImpl();
+		URI modelURI = URI.createURI("file:/C:/Users/schallit/runtime-EclipseApplication31072017/tosca_model/TOSCA.occie");
+		resource = resSet.createResource(modelURI);
+
+		extension = OCCIFactory.eINSTANCE.createExtension();
+		extension.setDescription("Mon extension TOSCA");
+		extension.setScheme("http://occi/tosca#");
+		extension.setName("TOSCA");
+		resource.getContents().add(extension);
+
+		coreExtension = OcciHelper.loadExtension("http://schemas.ogf.org/occi/core#");
+		if (coreExtension == null) {
+			throw new RuntimeException("Cannot load OCCI core extension!");
+		}
+
+		List<Kind> coreKinds = coreExtension.getKinds();
+		for (Kind coreKind : coreKinds) {
+			if (coreKind.getTerm().equals("resource")) {
+				resourceKind = coreKind;
+			} else if (coreKind.getTerm().equals("link")) {
+				linkKind = coreKind;
+			}
+		}
+		
+		infrastructureExtension = OcciHelper.loadExtension("http://schemas.ogf.org/occi/infrastructure#");
+		if (infrastructureExtension == null) {
+			throw new RuntimeException("Cannot load OCCI infrastructure extension!");
+		}
+		List<Kind> infrastructureKinds = infrastructureExtension.getKinds();
+		for (Kind infrastructureKind : infrastructureKinds) {
+			System.err.println(infrastructureKind.getTerm());
+			if (infrastructureKind.getTerm().equals("storage")) {
+				storageResourceInfrastructure = infrastructureKind;
+			}
+		}
+		
+		platformExtension = OcciHelper.loadExtension("http://schemas.ogf.org/occi/platform#");
+		if (platformExtension == null) {
+			throw new RuntimeException("Cannot load OCCI platform extension!");
+		}
+		List<Kind> platformKinds = platformExtension.getKinds();
+		for (Kind platformKind : platformKinds) {
+			System.err.println(platformKind.getTerm());
+			if (platformKind.getTerm().equals("component")) {
+				System.err.println("loaded");
+				componentResourcePlatform = platformKind;
+			}
+		}
+	}
+	
+	private Kind getKindByName(String name) {
+		for (Kind kind : extension.getKinds()) {
+			if (name.equals(kind.getName())) {
+				return kind;
+			}
+		}
+		Map<String, ?> maps = (Map<String, ?>)kinds.get(name);
+		System.out.println(name + " is not defined");
+		if (maps == null && StringToDataType.map.get(name) != null) {
+			// TODO for instance we have derived_from: integer
+			System.err.println(name);
+			return null;
+		} else {
+			readKind(name, maps);
+			return getKindByName(name);
+		}
+	}
+	
 	private void readKind(String kindStr, Map<String, ?> map) {
-		System.out.println(kindStr);
+		boolean alreadyRegistered = false;
+		for (Kind registeredKind : extension.getKinds()) {
+			if (registeredKind.getName().equals(kindStr)) {
+				alreadyRegistered = true;
+			}
+		}
+		if (alreadyRegistered) {
+			return;
+		}
+		
     	Kind kind = OCCIFactory.eINSTANCE.createKind();
     	kind.setName(kindStr);
+    	
+    	if (kindStr.equals("tosca.nodes.BlockStorage")) {
+    		kind.setParent(storageResourceInfrastructure);
+    	} else if (kindStr.equals("tosca.nodes.SoftwareComponent")){
+    		System.err.println(componentResourcePlatform);
+    		kind.setParent(componentResourcePlatform);
+    		Mixin softwareComponent = OCCIFactory.eINSTANCE.createMixin();
+    		softwareComponent.setName("softwareComponentMixin");
+    		softwareComponent.getApplies().add(kind);
+    		softwareComponent.setScheme("http://occi/tosca/"+"softwareComponentMixin".replaceAll("\\.", "").toLowerCase()+"#");
+    		extension.getMixins().add(softwareComponent);
+    	} else {
+	    	Object derived_from = map.get("derived_from");
+	    	if (derived_from != null) {
+	    		Kind parent = getKindByName(derived_from.toString());
+	    		if (parent == null) {
+	    			System.err.println(kindStr);
+	    		}
+	    		kind.setParent(parent);
+	    	} else if (kindStr.startsWith("tosca.relationships")) {
+	    		kind.setParent(linkKind);
+	    	}
+    	}
+    	
     	Object description = map.get("description");
     	if (description != null) {
     		kind.setTitle(description.toString());	
@@ -104,9 +216,42 @@ public class Main extends AbstractHandler {
     	if (attributes != null) {
     		readAttributes(kind, (Map<String, ?> )map.get("properties"));	
     	}
+   
+    	if (map.get("requirements") != null) {
+    		Map<String, ?> tmp = (Map<String, ?>)((List)map.get("requirements")).get(0);
+    		Map<String, ?> linkDefinition = ((Map<String, ?>)tmp.get(new ArrayList(tmp.keySet()).get(0)));
+    		if (linkDefinition.get("node") != null) {
+    			System.out.println(linkDefinition.get("node"));
+    			String linkName = (String)linkDefinition.get("relationship");
+    			Kind linkKind = getKindByName(linkName);
+    			if (linkKind.getSource() != null) {
+    				linkKind = copyKind(linkKind);
+    			}
+    			linkKind.setSource(kind);
+    			String targetName = (String)linkDefinition.get("node");
+    			if (targetName.equals(kindStr)) {
+    				linkKind.setTarget(kind); // here see the case of tosca.nodes.Root which is the source AND the target of a tosca.relationships.DependsOn Link
+    			} else {
+    				Kind targetKind = getKindByName(targetName);
+        			linkKind.setTarget(targetKind);
+    			}
+    			System.out.println(kind.getName() + " ---- " + linkName + " --> " + targetName);
+    		}
+    	}
+    	kind.setScheme("http://occi/tosca/"+kindStr.replaceAll("\\.", "").toLowerCase()+"#");
     	extension.getKinds().add(kind);
 	}
 
+	private Kind copyKind(Kind toBoCopied) {
+		Kind copy = OCCIFactory.eINSTANCE.createKind();
+		copy.setName(toBoCopied.getName());
+		copy.setScheme(toBoCopied.getScheme());
+		copy.setParent(toBoCopied.getParent());
+		copy.setTitle(toBoCopied.getTitle());
+		extension.getKinds().add(copy);
+		return copy;
+	}
+	
 	private void readAttributes(Kind kind, Map<String, ?> attributes) {
 		for (String attributeName : attributes.keySet()) {
 			Attribute attribute = OCCIFactory.eINSTANCE.createAttribute();
@@ -118,6 +263,9 @@ public class Main extends AbstractHandler {
 			}
 			if (attributesValues.containsKey("required")) {
 				attribute.setRequired(attributesValues.get("required").equals("true"));	
+			}
+			if (attributesValues.containsKey("default")) {
+				attribute.setDefault((String)attributesValues.get("default"));
 			}
 			DataType type = StringToDataType.map.get(attributesValues.get("type"));
 			if (type != null) {
