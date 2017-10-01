49071930cfe88214d48628a055c4cae3626cd1de
Stephanie Challita <stephanie.challita@inria.fr>
2017-10-02 00:01:03 +0200

    Add datatypes support

---
+++ src/org/occiware/clouddesigner/tosca/handlers/DataTypeReader.java
@@ -0,0 +1,70 @@
+package org.occiware.clouddesigner.tosca.handlers;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+
+import org.eclipse.cmf.occi.core.Kind;
+import org.eclipse.cmf.occi.core.OCCIFactory;
+import org.eclipse.cmf.occi.core.RecordField;
+import org.eclipse.cmf.occi.core.RecordType;
+
+public class DataTypeReader {
+	
+	public static void read(String dataTypeAsString, Map<String, ?> map) {
+		RecordType recordType = OCCIFactory.eINSTANCE.createRecordType();
+		recordType.setName(dataTypeAsString);
+		Object description = map.get("description");
+    	if (description != null) {
+    		recordType.setDocumentation(description.toString());	
+    	}
+    	// do not support the derived_from
+    	Map<String, ?> attributes = (Map<String, ?> )map.get("properties");
+    	if (attributes != null) {
+    		recordType.getRecordFields().addAll(readRecordField((Map<String, ?> )map.get("properties")));	
+    	}
+
+    	Main.extension.getTypes().add(recordType);
+	}
+	
+	private static Collection<RecordField> readRecordField(Map<String, ?> recordFieldsMap) {
+		List<RecordField> recordFields = new ArrayList<>();
+		for (String recordFieldName : recordFieldsMap.keySet()) {
+			RecordField recordField = OCCIFactory.eINSTANCE.createRecordField();
+			recordField.setName(recordFieldName);
+			Map<String, ?> recordFieldMap = (Map<String, ?>)recordFieldsMap.get(recordFieldName);
+			
+			String description = (String)recordFieldMap.get("description");
+			if (description != null) {
+				recordField.setDescription(description);	
+			}
+			
+			String type = (String)recordFieldMap.get("type");
+			if (type != null) {
+				if (type.equals("list")) {
+					type = "array";
+				}
+				if (type.equals("array") || type.equals("map")) {
+					String typeOfCollection = (String)(((Map<String, ?>)recordFieldMap.get("entry_schema")).get("type"));
+					type += typeOfCollection;                                                                                                                      
+				}
+				recordField.setType(StringToDataType.map.get(type));	
+			}
+			
+			String required = (String)recordFieldMap.get("required");
+			if (required != null) {
+				recordField.setRequired(required.equals("true"));	
+			}
+			
+			String defaultValue = (String)recordFieldMap.get("default");
+			if (defaultValue != null) {
+				recordField.setDefault(description);	
+			}
+			
+			recordFields.add(recordField);
+		}
+		return recordFields;
+	}
+ 
+}
--- src/org/occiware/clouddesigner/tosca/handlers/StringToDataType.java
+++ src/org/occiware/clouddesigner/tosca/handlers/StringToDataType.java
@@ -25,11 +25,25 @@ public class StringToDataType {
 	public final static NumericType typeModelInteger = OCCIFactory.eINSTANCE.createNumericType();
 	public final static NumericType typeModelLong = OCCIFactory.eINSTANCE.createNumericType();
 	public final static NumericType typeModelShort = OCCIFactory.eINSTANCE.createNumericType();
+	
 	public final static EnumerationType typeEnum = OCCIFactory.eINSTANCE.createEnumerationType();
+	
 	public final static ArrayType typeArray = OCCIFactory.eINSTANCE.createArrayType();
+	public final static ArrayType typeArrayString = OCCIFactory.eINSTANCE.createArrayType();
+	public final static ArrayType typeArrayBoolean = OCCIFactory.eINSTANCE.createArrayType();
+	public final static ArrayType typeArrayByte = OCCIFactory.eINSTANCE.createArrayType();
+	public final static ArrayType typeArrayDouble = OCCIFactory.eINSTANCE.createArrayType();
+	public final static ArrayType typeArrayFloat = OCCIFactory.eINSTANCE.createArrayType();
+	public final static ArrayType typeArrayInteger = OCCIFactory.eINSTANCE.createArrayType();
+	public final static ArrayType typeArrayLong = OCCIFactory.eINSTANCE.createArrayType();
+	public final static ArrayType typeArrayShort = OCCIFactory.eINSTANCE.createArrayType();
+	
+	
 	public final static RecordType typeRecord = OCCIFactory.eINSTANCE.createRecordType();
 	
 	public final static EObjectType typeModelMap = OCCIFactory.eINSTANCE.createEObjectType();
+	
+	public final static EObjectType typeModelMapString = OCCIFactory.eINSTANCE.createEObjectType();
 	
 	public final static Map<String, DataType> map = new HashMap<>();
 
@@ -37,23 +51,44 @@ public class StringToDataType {
 		typeModelByte.setType(NumericTypeEnum.BYTE);
 		typeModelByte.setName("byte");
 		typeModelDouble.setType(NumericTypeEnum.DOUBLE);
-		typeModelByte.setName("double");
+		typeModelDouble.setName("double");
 		typeModelFloat.setType(NumericTypeEnum.FLOAT);
-		typeModelByte.setName("float");
+		typeModelFloat.setName("float");
 		typeModelInteger.setType(NumericTypeEnum.INTEGER);
-		typeModelByte.setName("integer");
+		typeModelInteger.setName("integer");
 		typeModelLong.setType(NumericTypeEnum.LONG);
-		typeModelByte.setName("long");
+		typeModelLong.setName("long");
 		typeModelShort.setType(NumericTypeEnum.SHORT);
-		typeModelByte.setName("short");
+		typeModelShort.setName("short");
 		
 		typeModelBoolean.setName("boolean");
 		typeModelString.setName("string");
 		typeEnum.setName("enum");
+		
 		typeArray.setName("array");
+		
+		typeArrayString.setName("arrayString");
+		typeArrayString.setType(typeModelString);
+		typeArrayBoolean.setName("arrayBoolean");
+		typeArrayBoolean.setType(typeModelBoolean);
+		typeArrayByte.setName("arrayByte");
+		typeArrayByte.setType(typeModelByte);
+		typeArrayDouble.setName("arrayDouble");
+		typeArrayDouble.setType(typeModelDouble);
+		typeArrayFloat.setName("arrayFloat");
+		typeArrayFloat.setType(typeModelFloat);
+		typeArrayInteger.setName("arrayInteger");
+		typeArrayInteger.setType(typeModelInteger);
+		typeArrayLong.setName("arrayLong");
+		typeArrayLong.setType(typeModelLong);
+		typeArrayShort.setName("arrayShort");
+		typeArrayShort.setType(typeModelShort);
+		
 		typeRecord.setName("record");
 		
 		typeModelMap.setName("map");
+		
+		typeModelMapString.setName("mapString");
 		
 		Main.extension.getTypes().add(typeModelString);
 		Main.extension.getTypes().add(typeModelBoolean);
@@ -64,7 +99,17 @@ public class StringToDataType {
 		Main.extension.getTypes().add(typeModelLong);
 		Main.extension.getTypes().add(typeModelShort);
 		Main.extension.getTypes().add(typeEnum);
+		
 		Main.extension.getTypes().add(typeArray);
+		Main.extension.getTypes().add(typeArrayString);
+		Main.extension.getTypes().add(typeArrayBoolean);
+		Main.extension.getTypes().add(typeArrayByte);
+		Main.extension.getTypes().add(typeArrayDouble);
+		Main.extension.getTypes().add(typeArrayFloat);
+		Main.extension.getTypes().add(typeArrayInteger);
+		Main.extension.getTypes().add(typeArrayLong);
+		Main.extension.getTypes().add(typeArrayShort);
+		
 		Main.extension.getTypes().add(typeRecord);
 		
 		Main.extension.getTypes().add(typeModelMap);
@@ -83,8 +128,10 @@ public class StringToDataType {
 		
 		map.put("enum", typeEnum);
 		map.put("array", typeArray);
+		map.put("arraystring", typeArray);
 		map.put("record", typeRecord);
 		
 		map.put("map", typeModelMap);
+		map.put("mapString", typeModelMapString);
 	}
 }
