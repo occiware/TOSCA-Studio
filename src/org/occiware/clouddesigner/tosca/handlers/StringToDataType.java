09b14b6d7d1b3daa6090b7b80edbb28cad79db31
Stephanie Challita <stephanie.challita@inria.fr>
2017-10-19 22:51:20 +0200

    Add the declaration of stringMinOne, integerMinOne and 4 scalar types

--- src/org/occiware/clouddesigner/tosca/handlers/StringToDataType.java
+++ src/org/occiware/clouddesigner/tosca/handlers/StringToDataType.java
@@ -19,11 +19,13 @@ import org.eclipse.cmf.occi.core.StringType;
 public class StringToDataType {
 	
 	public final static StringType typeModelString = OCCIFactory.eINSTANCE.createStringType();
+	public final static StringType typeModelStringMinOne = OCCIFactory.eINSTANCE.createStringType();
 	public final static BooleanType typeModelBoolean = OCCIFactory.eINSTANCE.createBooleanType();
 	public final static NumericType typeModelByte = OCCIFactory.eINSTANCE.createNumericType();
 	public final static NumericType typeModelDouble = OCCIFactory.eINSTANCE.createNumericType();
 	public final static NumericType typeModelFloat = OCCIFactory.eINSTANCE.createNumericType();
 	public final static NumericType typeModelInteger = OCCIFactory.eINSTANCE.createNumericType();
+	public final static NumericType typeModelIntegerMinOne = OCCIFactory.eINSTANCE.createNumericType();
 	public final static NumericType typeModelLong = OCCIFactory.eINSTANCE.createNumericType();
 	public final static NumericType typeModelShort = OCCIFactory.eINSTANCE.createNumericType();
 	
@@ -49,9 +51,33 @@ public class StringToDataType {
 	
 	public final static StringType typeModelVersion = OCCIFactory.eINSTANCE.createStringType();
 	
+	public final static NumericType scalarFrequency = OCCIFactory.eINSTANCE.createNumericType();
+	
+	public final static NumericType scalarSizeZeroMB = OCCIFactory.eINSTANCE.createNumericType();
+	
+	public final static NumericType scalarSizeZeroGB = OCCIFactory.eINSTANCE.createNumericType();
+	
+	public final static NumericType scalarSizeOneMB = OCCIFactory.eINSTANCE.createNumericType();
+	
 	public final static Map<String, DataType> map = new HashMap<>();
 
 	static {
+		scalarFrequency.setType(NumericTypeEnum.DOUBLE);
+		scalarFrequency.setName("scalarFrequency");
+		scalarFrequency.setMinInclusive("0.1 GHz");
+		
+		scalarSizeZeroMB.setType(NumericTypeEnum.INTEGER);
+		scalarSizeZeroMB.setName("scalarSizeMinZeroMB");
+		scalarSizeZeroMB.setMinInclusive("0 MB");
+		
+		scalarSizeZeroGB.setType(NumericTypeEnum.INTEGER);
+		scalarSizeZeroGB.setName("scalarSizeMinZeroGB");
+		scalarSizeZeroGB.setMinInclusive("0 GB");
+		
+		scalarSizeOneMB.setType(NumericTypeEnum.INTEGER);
+		scalarSizeOneMB.setName("scalarSizeMinOneMB");
+		scalarSizeOneMB.setMinInclusive("1 MB");
+		
 		typeModelByte.setType(NumericTypeEnum.BYTE);
 		typeModelByte.setName("byte");
 		typeModelDouble.setType(NumericTypeEnum.DOUBLE);
@@ -60,6 +86,9 @@ public class StringToDataType {
 		typeModelFloat.setName("float");
 		typeModelInteger.setType(NumericTypeEnum.INTEGER);
 		typeModelInteger.setName("integer");
+		typeModelIntegerMinOne.setType(NumericTypeEnum.INTEGER);
+		typeModelIntegerMinOne.setName("integerMinOne");
+		typeModelIntegerMinOne.setMinInclusive("1");
 		typeModelLong.setType(NumericTypeEnum.LONG);
 		typeModelLong.setName("long");
 		typeModelShort.setType(NumericTypeEnum.SHORT);
@@ -72,6 +101,8 @@ public class StringToDataType {
 		
 		typeModelBoolean.setName("boolean");
 		typeModelString.setName("string");
+		typeModelStringMinOne.setName("stringMinOne");
+		typeModelStringMinOne.setMinLength(1);
 		typeEnum.setName("enum");
 		
 		typeArray.setName("array");
@@ -103,11 +134,13 @@ public class StringToDataType {
 		typeModelVersion.setPattern("((\\d+)((\\.\\d+)+)?)"); //examples: "10" "10.1" "10.1.999999", TODO check the pattern
 		
 		ExtensionsManager.getExtension("tosca").getTypes().add(typeModelString);
+		ExtensionsManager.getExtension("tosca").getTypes().add(typeModelStringMinOne);
 		ExtensionsManager.getExtension("tosca").getTypes().add(typeModelBoolean);
 		ExtensionsManager.getExtension("tosca").getTypes().add(typeModelByte);
 		ExtensionsManager.getExtension("tosca").getTypes().add(typeModelDouble);
 		ExtensionsManager.getExtension("tosca").getTypes().add(typeModelFloat);
 		ExtensionsManager.getExtension("tosca").getTypes().add(typeModelInteger);
+		ExtensionsManager.getExtension("tosca").getTypes().add(typeModelIntegerMinOne);
 		ExtensionsManager.getExtension("tosca").getTypes().add(typeModelLong);
 		ExtensionsManager.getExtension("tosca").getTypes().add(typeModelShort);
 		ExtensionsManager.getExtension("tosca").getTypes().add(typeEnum);
@@ -130,14 +163,24 @@ public class StringToDataType {
 		
 		ExtensionsManager.getExtension("tosca").getTypes().add(typeModelRange);
 		
+		ExtensionsManager.getExtension("tosca").getTypes().add(scalarFrequency);
+		
+		ExtensionsManager.getExtension("tosca").getTypes().add(scalarSizeOneMB);
+		
+		ExtensionsManager.getExtension("tosca").getTypes().add(scalarSizeZeroGB);
+		
+		ExtensionsManager.getExtension("tosca").getTypes().add(scalarSizeZeroMB);
+		
 		
 		map.put("string", typeModelString);
-		map.put("boolean", typeModelBoolean);	
+		map.put("stringMinOne", typeModelStringMinOne);
+		map.put("boolean", typeModelBoolean);
 		
 		map.put("byte", typeModelByte);
 		map.put("double", typeModelDouble);
 		map.put("float", typeModelFloat);
 		map.put("integer", typeModelInteger);
+		map.put("integer1", typeModelIntegerMinOne);
 		map.put("number", typeModelInteger);
 		map.put("uint32", typeModelInteger);
 		map.put("long", typeModelLong);
@@ -152,6 +195,13 @@ public class StringToDataType {
 		map.put("mapString", typeModelMapString);
 		map.put("version", typeModelVersion);
 		map.put("range", typeModelRange);
+
+		map.put("scalar-unit.frequency0.1 GHz", scalarFrequency);
 		
+		map.put("scalar-unit.size1 MB", scalarSizeOneMB);
+		map.put("scalar-unit.size0 GB", scalarSizeZeroGB);
+		map.put("scalar-unit.size0 MB", scalarSizeZeroMB);
 	}
 }
+
+
