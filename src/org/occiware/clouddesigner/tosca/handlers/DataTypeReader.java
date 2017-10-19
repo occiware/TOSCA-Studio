c7faf8abb2162b1270aae2670f27720cc936fb73
Stephanie Challita <stephanie.challita@inria.fr>
2017-10-19 22:37:46 +0200

    Parse correctly a record field which is an enumeration

--- src/org/occiware/clouddesigner/tosca/handlers/DataTypeReader.java
+++ src/org/occiware/clouddesigner/tosca/handlers/DataTypeReader.java
@@ -5,67 +5,92 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 
+import org.eclipse.cmf.occi.core.DataType;
+import org.eclipse.cmf.occi.core.EnumerationLiteral;
+import org.eclipse.cmf.occi.core.EnumerationType;
 import org.eclipse.cmf.occi.core.Kind;
 import org.eclipse.cmf.occi.core.OCCIFactory;
 import org.eclipse.cmf.occi.core.RecordField;
 import org.eclipse.cmf.occi.core.RecordType;
 
 public class DataTypeReader {
-	
+
 	public static void read(String dataTypeAsString, Map<String, ?> map) {
 		RecordType recordType = OCCIFactory.eINSTANCE.createRecordType();
 		recordType.setName(dataTypeAsString);
 		Object description = map.get("description");
-    	if (description != null) {
-    		recordType.setDocumentation(description.toString());	
-    	}
-    	// do not support the derived_from
-    	Map<String, ?> attributes = (Map<String, ?> )map.get("properties");
-    	if (attributes != null) {
-    		recordType.getRecordFields().addAll(readRecordField((Map<String, ?> )map.get("properties")));	
-    	}
+		if (description != null) {
+			recordType.setDocumentation(description.toString());
+		}
+		// do not support the derived_from
+		Map<String, ?> attributes = (Map<String, ?>) map.get("properties");
+		if (attributes != null) {
+			recordType.getRecordFields().addAll(readRecordField((Map<String, ?>) map.get("properties")));
+		}
 
-    	ExtensionsManager.getExtension("tosca").getTypes().add(recordType);
-    	StringToDataType.map.put(dataTypeAsString, recordType);
+		ExtensionsManager.getExtension("tosca").getTypes().add(recordType);
+		StringToDataType.map.put(dataTypeAsString, recordType);
 	}
-	
+
 	private static Collection<RecordField> readRecordField(Map<String, ?> recordFieldsMap) {
 		List<RecordField> recordFields = new ArrayList<>();
 		for (String recordFieldName : recordFieldsMap.keySet()) {
 			RecordField recordField = OCCIFactory.eINSTANCE.createRecordField();
 			recordField.setName(recordFieldName);
-			Map<String, ?> recordFieldMap = (Map<String, ?>)recordFieldsMap.get(recordFieldName);
-			
-			String description = (String)recordFieldMap.get("description");
+			Map<String, ?> recordFieldMap = (Map<String, ?>) recordFieldsMap.get(recordFieldName);
+
+			String description = (String) recordFieldMap.get("description");
 			if (description != null) {
-				recordField.setDescription(description);	
+				recordField.setDescription(description);
 			}
-			
-			String type = (String)recordFieldMap.get("type");
+
+			String type = (String) recordFieldMap.get("type");
 			if (type != null) {
 				if (type.equals("list")) {
 					type = "array";
 				}
 				if (type.equals("array")) {
-					String typeOfCollection = (String)(((Map<String, ?>)recordFieldMap.get("entry_schema")).get("type"));
-					type += typeOfCollection;                                                                                                                      
+					String typeOfCollection = (String) (((Map<String, ?>) recordFieldMap.get("entry_schema"))
+							.get("type"));
+					type += typeOfCollection;
 				}
-				recordField.setType(StringToDataType.map.get(type));	
+				List<Map<String, ?>> constraints = (ArrayList<Map<String, ?>>) recordFieldMap.get("constraints");
+				if (constraints != null) {
+					for (Map<String, ?> constraint : constraints) {
+						if (constraint.get("valid_values") != null) {
+							List<String> literals = (ArrayList) constraint.get("valid_values");
+							DataType enumType = OCCIFactory.eINSTANCE.createEnumerationType();
+							enumType.setName(recordFieldName + "Enum");
+							for (String literal : literals) {
+								EnumerationLiteral enumLit = OCCIFactory.eINSTANCE.createEnumerationLiteral();
+								enumLit.setName(literal);
+								enumLit.setEnumerationType((EnumerationType) enumType);
+								((EnumerationType) enumType).getLiterals().add(enumLit);
+							}
+							StringToDataType.map.put(recordFieldName + "Enum", enumType);
+							ExtensionsManager.getExtension("tosca").getTypes().add(enumType);
+							recordField.setType(enumType);
+						}
+					}
+				} else {
+					recordField.setType(StringToDataType.map.get(type));
+				}
+
 			}
-			
-			String required = (String)recordFieldMap.get("required");
+
+			String required = (String) recordFieldMap.get("required");
 			if (required != null) {
-				recordField.setRequired(required.equals("true"));	
+				recordField.setRequired(required.equals("true"));
 			}
-			
-			String defaultValue = (String)recordFieldMap.get("default");
+
+			String defaultValue = (String) recordFieldMap.get("default");
 			if (defaultValue != null) {
-				recordField.setDefault(description);	
+				recordField.setDefault(description);
 			}
-			
+
 			recordFields.add(recordField);
 		}
 		return recordFields;
 	}
- 
+
 }
