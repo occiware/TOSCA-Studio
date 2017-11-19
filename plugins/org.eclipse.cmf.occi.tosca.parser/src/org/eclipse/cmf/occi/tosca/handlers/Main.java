60595ce12859c5eb1517deff5a3d74f4615b7e59
Stephanie Challita <stephanie.challita@inria.fr>
2017-11-20 00:08:24 +0100

    Create custom-types.yml then parse it as Mixins to build extendedTosca.occie

--- plugins/org.eclipse.cmf.occi.tosca.parser/src/org/eclipse/cmf/occi/tosca/handlers/Main.java
+++ plugins/org.eclipse.cmf.occi.tosca.parser/src/org/eclipse/cmf/occi/tosca/handlers/Main.java
@@ -1,33 +1,25 @@
 
 package org.eclipse.cmf.occi.tosca.handlers;
 
+import java.io.File;
 import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
+import java.io.FileWriter;
+import java.io.FilenameFilter;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
-import org.eclipse.cmf.occi.core.Attribute;
-import org.eclipse.cmf.occi.core.DataType;
-import org.eclipse.cmf.occi.core.EObjectType;
 import org.eclipse.cmf.occi.core.Extension;
 import org.eclipse.cmf.occi.core.Kind;
 import org.eclipse.cmf.occi.core.Mixin;
-import org.eclipse.cmf.occi.core.OCCIFactory;
-import org.eclipse.cmf.occi.core.util.OcciHelper;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
-import org.eclipse.emf.common.util.URI;
-import org.eclipse.emf.ecore.resource.Resource;
-import org.eclipse.emf.ecore.resource.ResourceSet;
-import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.handlers.HandlerUtil;
 
 import com.esotericsoftware.yamlbeans.YamlReader;
+import com.esotericsoftware.yamlbeans.YamlWriter;
 
 /**
  * Our sample handler extends AbstractHandler, an IHandler base class.
@@ -35,72 +27,23 @@ import com.esotericsoftware.yamlbeans.YamlReader;
  * @see org.eclipse.core.commands.IHandler
  * @see org.eclipse.core.commands.AbstractHandler
  */
+@SuppressWarnings({ "unchecked", "rawtypes" })
 public class Main extends AbstractHandler {
-
-	private Map<String, ?> map;
 
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
-		IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindowChecked(event);
-		// MessageDialog.openInformation(window.getShell(), "Tosca", "Tosca2Occi");
+		// IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindowChecked(event);
 		try {
 			YamlReader reader = new YamlReader(new FileReader(
-					"C:/Users/schallit/workspace-tosca/plugins/org.eclipse.cmf.occi.tosca.parser/normative-types.yml"));
-			map = (Map<String, ?>) reader.read();
+					"C:/Users/schallit/workspace-tosca/plugins/org.eclipse.cmf.occi.tosca.parser/tosca-types/normative-types.yml"));
+			Map<String, ?> normativeTypesMap = (Map<String, ?>) reader.read();
+			readYamlFile(normativeTypesMap);
+			executeMapping();
+			ExtensionsManager.save();
 
-			// datatypes <=> record
-			Map<String, ?> dataTypes = (Map<String, ?>) map.get("data_types");
-			for (String datatype : dataTypes.keySet()) {
-				if (!"tosca.datatypes.Root".equals(datatype) && //the datatype Root is omitted because it does not have any record fields, and it is not allowed in OCCI meta model
-						((Map<String, ?>) dataTypes.get(datatype)).get("properties") != null) {//tosca.datatypes.network.PortDef is also omitted and considered as a type
-					DataTypeReader.read(datatype, (Map<String, ?>) dataTypes.get(datatype));
-				}
-			}
-
-			// capabilities
-			Map<String, ?> capabilites = (Map<String, ?>) map.get("capability_types");
-			MixinReader mixinReader = new MixinReader(capabilites);
-			for (String capability : capabilites.keySet()) {
-				mixinReader.readMixin(capability, (Map<String, ?>) capabilites.get(capability));
-			}
-
-			for (String collection : map.keySet()) {
-				if (collection.endsWith("_types") && !"relationship_types".equals(collection)
-						&& !"data_types".equals(collection) && !"artifact_types".equals(collection)
-						&& !"capability_types".equals(collection)) {
-					Map<String, ?> mixins = (Map<String, ?>) map.get(collection);
-					mixinReader = new MixinReader(mixins);
-					for (String mixin : mixins.keySet()) {
-						mixinReader.readMixin(mixin, (Map<String, ?>) mixins.get(mixin));
-					}
-				}
-			}
-
-			// links / relationship
-			Map<String, ?> links = (Map<String, ?>) map.get("relationship_types");
-			mixinReader = new MixinReader(links);
-			for (String link : links.keySet()) {
-				mixinReader.readMixin(link, (Map<String, ?>) links.get(link));
-			}
-
-			Extension extension = ExtensionsManager.getExtension("tosca");
-			Mapper mapper = new TypeMapper();
-			for (Kind kind : extension.getKinds()) {
-				mapper.map(kind);
-			}
-
-			for (Mixin mixin : extension.getMixins()) {
-				mapper.map(mixin);
-			}
-
-			mapper = new AttributeMapper();
-			for (Kind kind : extension.getKinds()) {
-				mapper.map(kind);
-			}
-			for (Mixin mixin : extension.getMixins()) {
-				mapper.map(mixin);
-			}
-
+			ExtensionsManager.createExtendedTosca();
+			Map<String, ?> customTypesMap = concatAllCustomTypes();
+			readYamlFile(customTypesMap);
 			ExtensionsManager.save();
 
 		} catch (Exception e) {
@@ -108,4 +51,106 @@ public class Main extends AbstractHandler {
 		}
 		return null;
 	}
+
+	private void readYamlFile(Map<String, ?> map) {
+		// datatypes <=> record
+		Map<String, ?> dataTypes = (Map<String, ?>) map.get("data_types");
+		if (dataTypes != null) {
+			for (String datatype : dataTypes.keySet()) {
+				if (!"tosca.datatypes.Root".equals(datatype) && // the datatype Root is omitted because it does not have
+																// any record fields, and it is not allowed in OCCI meta
+																// model
+						((Map<String, ?>) dataTypes.get(datatype)).get("properties") != null) {// tosca.datatypes.network.PortDef
+																								// is also omitted and
+																								// considered as a type
+					DataTypeReader.read(datatype, (Map<String, ?>) dataTypes.get(datatype));
+				}
+			}
+		}
+
+		// capabilities
+		Map<String, ?> capabilites = (Map<String, ?>) map.get("capability_types");
+		MixinReader mixinReader = new MixinReader(ExtensionsManager.getExtension("tosca"), capabilites);
+		for (String capability : capabilites.keySet()) {
+			mixinReader.readMixin(capability, (Map<String, ?>) capabilites.get(capability));
+		}
+
+		for (String collection : map.keySet()) {
+			if (collection.endsWith("_types") && !"relationship_types".equals(collection)
+					&& !"data_types".equals(collection) && !"artifact_types".equals(collection)
+					&& !"capability_types".equals(collection)) {
+				Map<String, ?> mixins = (Map<String, ?>) map.get(collection);
+				mixinReader = new MixinReader(ExtensionsManager.getExtension("tosca"), mixins);
+				for (String mixinStr : mixins.keySet()) {
+					mixinReader.readMixin(mixinStr, (Map<String, ?>) mixins.get(mixinStr));
+				}
+			}
+		}
+
+		// links / relationship
+		Map<String, ?> links = (Map<String, ?>) map.get("relationship_types");
+		mixinReader = new MixinReader(ExtensionsManager.getExtension("tosca"), links);
+		for (String link : links.keySet()) {
+			mixinReader.readMixin(link, (Map<String, ?>) links.get(link));
+		}
+	}
+
+	private void executeMapping() {
+		Extension extension = ExtensionsManager.getExtension("tosca");
+		Mapper mapper = new TypeMapper();
+		for (Kind kind : extension.getKinds()) {
+			mapper.map(kind);
+		}
+
+		for (Mixin mixin : extension.getMixins()) {
+			mapper.map(mixin);
+		}
+
+		mapper = new AttributeMapper();
+		for (Kind kind : extension.getKinds()) {
+			mapper.map(kind);
+		}
+		for (Mixin mixin : extension.getMixins()) {
+			mapper.map(mixin);
+		}
+	}
+
+	private Map<String, ?> concatAllCustomTypes() throws Exception {
+		Map customTypesMap = new LinkedHashMap<>(); // LinkedHashMap keeps the insertion order 
+		Map nodes = new HashMap<>();
+		Map capabilities = new HashMap<>();
+		Map relationships = new HashMap<>();
+		Map policies = new HashMap<>();
+		File directory = new File(
+				"C:/Users/schallit/workspace-tosca/plugins/org.eclipse.cmf.occi.tosca.parser/tosca-types/custom-types/");
+		File[] yamlFiles = directory.listFiles();
+		for (File path : yamlFiles) {
+			YamlReader reader = new YamlReader(new FileReader(path));
+			Map<String, ?> map = (Map<String, ?>) reader.read();
+			nodes.putAll((Map) map.get("node_types"));
+			if (map.containsKey("capability_types")) {
+				capabilities.putAll((Map) map.get("capability_types"));
+			}
+			if (map.containsKey("relationship_types")) {
+				relationships.putAll((Map) map.get("relationship_types"));
+			}
+			if (map.containsKey("policy_types")) {
+				policies.putAll((Map) map.get("policy_types"));
+			}
+			reader.close();
+		}
+		customTypesMap.put("tosca_definitions_version", "tosca_simple_yaml_1_0");
+		customTypesMap.put("description",
+				"This TOSCA definitions document contains the custom types definitions as expressed in the TOSCA specification document. It is composed by the files in the directory custom-types");
+		customTypesMap.put("node_types", nodes);
+		customTypesMap.put("capability_types", capabilities);
+		customTypesMap.put("relationship_types", relationships);
+		customTypesMap.put("policy_types", policies);
+		YamlWriter writerNodes = new YamlWriter(new FileWriter(
+				"C:/Users/schallit/workspace-tosca/plugins/org.eclipse.cmf.occi.tosca.parser/tosca-types/custom-types.yml"));
+		writerNodes.write(customTypesMap);
+		writerNodes.close();
+		return customTypesMap;
+	}
+
 }
